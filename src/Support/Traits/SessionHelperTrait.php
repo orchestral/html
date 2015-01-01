70f6216f1012122790bc71d6be08882be2fc2691
crynobone <crynobone@gmail.com>
2015-01-02 01:07:20 +0800

    Adding more trait.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Support/Traits/SessionHelperTrait.php
+++ src/Support/Traits/SessionHelperTrait.php
@@ -12,6 +12,29 @@ trait SessionHelperTrait
     protected $session;
 
     /**
+     * Get a value from the session's old input.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    public function old($name)
+    {
+        if (isset($this->session)) {
+            return $this->session->getOldInput($this->transformKey($name));
+        }
+    }
+
+    /**
+     * Determine if the old input is empty.
+     *
+     * @return bool
+     */
+    public function oldInputIsEmpty()
+    {
+        return (isset($this->session) && count($this->session->getOldInput()) == 0);
+    }
+
+    /**
      * Get the session store implementation.
      *
      * @return  \Illuminate\Session\Store  $session
@@ -33,4 +56,12 @@ trait SessionHelperTrait
 
         return $this;
     }
+
+    /**
+     * Transform key from array to dot syntax.
+     *
+     * @param  string  $key
+     * @return string
+     */
+    protected abstract function transformKey($key);
 }
---
+++ src/Support/Traits/TextInputTrait.php
@@ -0,0 +1,178 @@
+<?php namespace Orchestra\Html\Support\Traits;
+
+trait TextInputTrait
+{
+    /**
+     * Create a form input field.
+     *
+     * @param  string  $type
+     * @param  string  $name
+     * @param  string  $value
+     * @param  array   $options
+     * @return string
+     */
+    public function input($type, $name, $value = null, $options = [])
+    {
+        ! isset($options['name']) && $options['name'] = $name;
+
+        // We will get the appropriate value for the given field. We will look for the
+        // value in the session for the value in the old input data then we'll look
+        // in the model instance if one is set. Otherwise we will just use empty.
+        $id = $this->getIdAttribute($name, $options);
+
+        if (! in_array($type, $this->skipValueTypes)) {
+            $value = $this->getValueAttribute($name, $value);
+        }
+
+        // Once we have the type, value, and ID we can merge them into the rest of the
+        // attributes array so we can convert them into their HTML attribute format
+        // when creating the HTML element. Then, we will return the entire input.
+        $merge = compact('type', 'value', 'id');
+
+        $options = array_merge($options, $merge);
+
+        return '<input'.$this->html->attributes($options).'>';
+    }
+
+    /**
+     * Create a text input field.
+     *
+     * @param  string  $name
+     * @param  string  $value
+     * @param  array   $options
+     * @return string
+     */
+    public function text($name, $value = null, $options = [])
+    {
+        return $this->input('text', $name, $value, $options);
+    }
+
+    /**
+     * Create a password input field.
+     *
+     * @param  string  $name
+     * @param  array   $options
+     * @return string
+     */
+    public function password($name, $options = [])
+    {
+        return $this->input('password', $name, '', $options);
+    }
+
+    /**
+     * Create a hidden input field.
+     *
+     * @param  string  $name
+     * @param  string  $value
+     * @param  array   $options
+     * @return string
+     */
+    public function hidden($name, $value = null, $options = [])
+    {
+        return $this->input('hidden', $name, $value, $options);
+    }
+
+    /**
+     * Create an e-mail input field.
+     *
+     * @param  string  $name
+     * @param  string  $value
+     * @param  array   $options
+     * @return string
+     */
+    public function email($name, $value = null, $options = [])
+    {
+        return $this->input('email', $name, $value, $options);
+    }
+
+    /**
+     * Create a url input field.
+     *
+     * @param  string  $name
+     * @param  string  $value
+     * @param  array   $options
+     * @return string
+     */
+    public function url($name, $value = null, $options = [])
+    {
+        return $this->input('url', $name, $value, $options);
+    }
+
+    /**
+     * Create a file input field.
+     *
+     * @param  string  $name
+     * @param  array   $options
+     * @return string
+     */
+    public function file($name, $options = [])
+    {
+        return $this->input('file', $name, null, $options);
+    }
+
+    /**
+     * Create a textarea input field.
+     *
+     * @param  string  $name
+     * @param  string  $value
+     * @param  array   $options
+     * @return string
+     */
+    public function textarea($name, $value = null, $options = [])
+    {
+        ! isset($options['name']) && $options['name'] = $name;
+
+        // Next we will look for the rows and cols attributes, as each of these are put
+        // on the textarea element definition. If they are not present, we will just
+        // assume some sane default values for these attributes for the developer.
+        $options = $this->setTextAreaSize($options);
+
+        $options['id'] = $this->getIdAttribute($name, $options);
+
+        $value = (string) $this->getValueAttribute($name, $value);
+
+        unset($options['size']);
+
+        // Next we will convert the attributes into a string form. Also we have removed
+        // the size attribute, as it was merely a short-cut for the rows and cols on
+        // the element. Then we'll create the final textarea elements HTML for us.
+        $options = $this->html->attributes($options);
+
+        return '<textarea'.$options.'>'.e($value).'</textarea>';
+    }
+
+    /**
+     * Set the text area size on the attributes.
+     *
+     * @param  array  $options
+     * @return array
+     */
+    protected function setTextAreaSize($options)
+    {
+        if (isset($options['size'])) {
+            return $this->setQuickTextAreaSize($options);
+        }
+
+        // If the "size" attribute was not specified, we will just look for the regular
+        // columns and rows attributes, using sane defaults if these do not exist on
+        // the attributes array. We'll then return this entire options array back.
+        $cols = Arr::get($options, 'cols', 50);
+
+        $rows = Arr::get($options, 'rows', 10);
+
+        return array_merge($options, compact('cols', 'rows'));
+    }
+
+    /**
+     * Set the text area size using the quick "size" attribute.
+     *
+     * @param  array  $options
+     * @return array
+     */
+    protected function setQuickTextAreaSize($options)
+    {
+        $segments = explode('x', $options['size']);
+
+        return array_merge($options, array('cols' => $segments[0], 'rows' => $segments[1]));
+    }
+}
