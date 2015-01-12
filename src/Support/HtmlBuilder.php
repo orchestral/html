a0a69986e480176b683571e0cdaa3865e3274a06
crynobone <crynobone@gmail.com>
2015-01-13 07:55:16 +0800

    Add ObfuscateTrait.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Support/HtmlBuilder.php
+++ src/Support/HtmlBuilder.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Traits\Macroable;
+use Orchestra\Html\Support\Traits\ObfuscateTrait;
 
 class HtmlBuilder
 {
-    use Macroable;
+    use Macroable, ObfuscateTrait;
 
     /**
      * The URL generator instance.
@@ -338,38 +339,5 @@ class HtmlBuilder
         if (! is_null($value)) {
             return $key.'="'.e($value).'"';
         }
-    }
-
-    /**
-     * Obfuscate a string to prevent spam-bots from sniffing it.
-     *
-     * @param  string  $value
-     * @return string
-     */
-    public function obfuscate($value)
-    {
-        $safe = '';
-
-        foreach (str_split($value) as $letter) {
-            if (ord($letter) > 128) {
-                return $letter;
-            }
-
-            // To properly obfuscate the value, we will randomly convert each letter to
-            // its entity or hexadecimal representation, keeping a bot from sniffing
-            // the randomly obfuscated letters out of the string on the responses.
-            switch (rand(1, 3)) {
-                case 1:
-                    $safe .= '&#'.ord($letter).';';
-                    break;
-                case 2:
-                    $safe .= '&#x'.dechex(ord($letter)).';';
-                    break;
-                case 3:
-                    $safe .= $letter;
-            }
-        }
-
-        return $safe;
     }
 }
