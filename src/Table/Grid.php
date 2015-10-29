717e17491c9e44ca842558cfb1a959ed6efe0896
Steve Bauman <steven_bauman@outlook.com>
2015-10-29 13:42:42 -0400

    Use `is_callable()` instead of `method_exists()`

--- src/Table/Grid.php
+++ src/Table/Grid.php
@@ -411,7 +411,7 @@ class Grid extends BaseGrid implements GridContract
      */
     protected function buildModel($model)
     {
-        if ($this->isEloquentModel($model) && $this->paginate) {
+        if ($this->paginate === true && is_callable([$model, 'paginate'])) {
             $model = $model->paginate($this->perPage, ['*'], $this->pageName);
         } elseif ($this->isQueryBuilder($model)) {
             $model = $model->get();
