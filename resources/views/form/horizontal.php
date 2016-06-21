79d09fbc5115f3da7434bccdb6b1b9bebd43ba27
crynobone <crynobone@gmail.com>
2016-06-21 19:23:52 +0800

    Working on views.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/form/horizontal.php
+++ resources/views/form/horizontal.php
@@ -2,43 +2,42 @@
 
 echo Form::open(array_merge($grid->attributes(), ['class' => 'form-horizontal']));
 
-if ($token) echo csrf_field();
+if ($token) :
+echo csrf_field();
+endif;
 
-foreach ($grid->hiddens() as $hidden) echo $hidden;
+foreach ($grid->hiddens() as $hidden) :
+echo $hidden;
+endforeach;
 
 foreach ($grid->fieldsets() as $fieldset) : ?>
+  <fieldset<?php echo HTML::attributes($fieldset->attributes() ?: []); ?>>
+    <?php if ($fieldset->name) : ?>
+    <legend><?php echo e($fieldset->name) ?: '' ?></legend>
+    <?php endif; ?>
 
-    <fieldset<?php echo HTML::attributes($fieldset->attributes() ?: []); ?>>
-
-        <?php if ($fieldset->name) : ?><legend><?php echo e($fieldset->name) ?: '' ?></legend><?php endif; ?>
-
-        <?php foreach ($fieldset->controls() as $control) : ?>
-
-        <div class="form-group<?php echo $errors->has($control->id) ? ' has-error' : '' ?>">
-            <?php echo Form::label($control->name, $control->label, ['class' => 'three columns control-label']); ?>
-
-            <div class="nine columns">
-                <?php echo $control->getField($grid->data(), $control, []); ?>
-                <?php if ($control->inlineHelp) : ?><span class="help-inline"><?php echo $control->inlineHelp; ?></span><?php endif; ?>
-                <?php if ($control->help) : ?><p class="help-block"><?php echo $control->help; ?></p><?php endif; ?>
-                <?php echo $errors->first($control->id, $format); ?>
-            </div>
-        </div>
-
-        <?php endforeach; ?>
-
-    </fieldset>
+    <?php foreach ($fieldset->controls() as $control) : ?>
+    <div class="form-group<?php echo $errors->has($control->id) ? ' has-error' : '' ?>">
+      <?php echo Form::label($control->name, $control->label, ['class' => 'col-md-3 control-label']); ?>
+      <div class="col-md-9">
+        <?php echo $control->getField($grid->data(), $control->attributes(['class' => 'col-md-12']), []); ?>
+        <?php if ($control->inlineHelp) : ?>
+        <span class="help-inline"><?php echo $control->inlineHelp; ?></span>
+        <?php endif; ?>
+        <?php if ($control->help) : ?>
+        <p class="help-block"><?php echo $control->help; ?></p>
+        <?php endif; ?>
+        <?php echo $errors->first($control->id, $format); ?>
+      </div>
+    </div>
+    <?php endforeach; ?>
+  </fieldset>
 <?php endforeach; ?>
 
-<fieldset>
-    <div class="row">
-        <?php /* Fixed row issue on Bootstrap 3 */ ?>
-    </div>
-    <div class="row">
-        <div class="nine columns offset-by-three">
-            <button type="submit" class="btn btn-primary"><?php echo $submit; ?></button>
-        </div>
-    </div>
-</fieldset>
+<div class="row">
+  <div<?php echo HTML::attributable(array_get($meta, 'button', []), ['class' => 'col-md-9 col-md-offset-3']); ?>>
+    <button type="submit" class="btn btn-primary"><?php echo $submit; ?></button>
+  </div>
+</div>
 
 <?php echo Form::close(); ?>
